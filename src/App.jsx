3a29ca2f22d37dffbb5eb39e04eb907805bb35dd
IvoryWatkins <ivory.watkins23@gmail.com>
2024-02-20 10:11:04 -0600

    adding assignment, got to the Select a feature puppy tab.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-
+import './App.css'
+import{useState} from 'react'
+import {puppyList} from './data.js'
 
 function App() {
+const [puppies, setPuppies] = useState(puppyList)
+const [featPupId, setFeatPupId] = useState(null)
 
+const featuredPuppy = puppies.find((puppy) => puppy.id === featPupId)
+console.log('featuredPuppy', featuredPuppy)
+const something = featPupId && 'something'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+return (  
+  <div className='App'>
+       {featPupId &&
+          <div>
+              <h2>{featuredPuppy.name}</h2>
+              <ul>
+                <li>Age{puppy.age}</li>
+                <li>Email{puppy.email}</li>
+              </ul>
+          </div>
+        }
+        {
+          puppies.map((puppy) => {
+            return (
+            <p onClick={() => setFeatPupId(puppy.id)}
+             key = {puppy.id}>{puppy.name}</p>
+            );
+          })}
+          
+        </div>
     
-    </>
-  )
+  );
 }
 
 export default App
